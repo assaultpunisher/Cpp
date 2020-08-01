0ba63ca97b1bd5ebdaf42556878b9f2940cc6520
Yugam <yugamofficial@gmail.com>
2020-08-01 16:59:07 +0530

    cpp

---
+++ Basic Programs/P23_MultiplyTwoNumbers.cpp
@@ -0,0 +1,20 @@
+/*
+  Author: YUGAM
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double firstNumber, secondNumber, productofTwoNumbers;
+	
+	cout << "Enter two numbers : ";
+	cin >> firstNumber >> secondNumber;
+	
+	productofTwoNumbers = firstNumber * secondNumber;
+	
+	cout << "Product = " << productofTwoNumbers;
+	
+	return 0;
+}
