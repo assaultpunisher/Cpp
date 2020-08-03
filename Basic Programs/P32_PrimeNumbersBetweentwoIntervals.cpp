f136bed03a824387a2b5bda91962d8282fdf2045
Yugam <yugamofficial@gmail.com>
2020-08-03 22:33:58 +0530

    cpp

---
+++ Basic Programs/P32_PrimeNumbersBetweentwoIntervals.cpp
@@ -0,0 +1,43 @@
+/*
+  Author: YUGAM
+*/
+
+#include<iostream>
+using namespace std;
+
+int checkPrimeNumber(int);
+
+int main()
+{
+	int n1, n2;
+	bool flag;
+	
+	cout << "Enter two positive numbers : ";
+	cin >> n1 >> n2;
+	
+	cout << "Prime numbers between " << n1 << " and " << n2 << " are : " << endl;
+	
+	for(int i = n1+1; i < n2; ++i)
+	{
+		flag = checkPrimeNumber(i);
+		
+		if(flag)
+			cout << i << " ";
+	}
+	return 0;
+}
+
+int checkPrimeNumber(int n)
+{
+	bool flag = true;
+	
+	for(int j = 2; j <= n/2; ++j)
+	{
+		if(n % j == 0)
+		{
+			flag = false;
+			break;
+		}
+	}
+	return flag;
+}
