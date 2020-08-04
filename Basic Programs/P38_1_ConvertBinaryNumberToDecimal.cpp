7aeee0524e17884bec4db7aef1ac17db0444ae14
Yugam <yugamofficial@gmail.com>
2020-08-04 22:30:40 +0530

    cpp

---
+++ Basic Programs/P38_1_ConvertBinaryNumberToDecimal.cpp
@@ -0,0 +1,34 @@
+/*
+  Author: YUGAM
+*/
+
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int convertBinaryToDecimal(long long);
+
+int main()
+{
+	long long n;
+	
+	cout << "Enter a binary number : ";
+	cin >> n;
+	
+	cout << n << " in decimal = " << convertBinaryToDecimal(n) ;
+	return 0;
+}
+
+int convertBinaryToDecimal(long long n)
+{
+	int decimalNumber = 0, i = 0, remainder;
+	while (n != 0)
+	{
+		remainder = n % 10;
+		n /= 10;
+		decimalNumber += remainder*pow(2,i);
+		++i;
+	}
+	return decimalNumber;
+}
