3e1e5aa062a84cc41a034d8052b52b80a6ce6822
Yugam <yugamofficial@gmail.com>
2020-08-04 22:30:53 +0530

    cpp

---
+++ Basic Programs/P39_1_OctalNumbertoDecimal.cpp
@@ -0,0 +1,35 @@
+/*
+  Author: YUGAM
+*/
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int octalToDecimal(int octalNumber);
+
+int main()
+{
+	int octalNumber;
+	
+	cout << "Enter an octal number : ";
+	cin >> octalNumber;
+	
+	cout << octalNumber << " in decimal = " << octalToDecimal(octalNumber);
+	
+	return 0;
+}
+
+int octalToDecimal(int octalNumber)
+{
+	int decimalNumber = 0, i = 0, rem;
+	
+	while (octalNumber != 0)
+	{
+		rem = octalNumber % 10;
+		octalNumber /= 10;
+		decimalNumber += rem * pow(8, i);
+		++i;
+	}
+	return decimalNumber;
+}
