d436116fe075fa8f0eaa0eb6d7229cd158f9c816
Yugam <yugamofficial@gmail.com>
2020-08-05 22:41:12 +0530

    cpp

---
+++ Basic Programs/P40_2_Conver OctaltoBinary.cpp
@@ -0,0 +1,44 @@
+/*
+  Author: YUGAM
+*/
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long octalToBinary(int);
+int main()
+{
+    int octalNumber;
+
+    cout << "Enter an octal number: ";
+    cin >> octalNumber;
+
+    cout << octalNumber << " in binary = " << octalToBinary(octalNumber);
+
+    return 0;
+}
+
+long long octalToBinary(int octalNumber)
+{
+    int decimalNumber = 0, i = 0;
+    long long binaryNumber = 0;
+
+    while(octalNumber != 0)
+    {
+        decimalNumber += (octalNumber%10) * pow(8,i);
+        ++i;
+        octalNumber/=10;
+    }
+
+    i = 1;
+
+    while (decimalNumber != 0)
+    {
+        binaryNumber += (decimalNumber % 2) * i;
+        decimalNumber /= 2;
+        i *= 10;
+    }
+
+    return binaryNumber;
+}
