ae02182f4dfb5507cb0a8ab028335a94da3c1e19
Yugam <yugamofficial@gmail.com>
2020-08-09 12:12:21 +0530

    cpp

---
+++ Basic Programs/P55_1_LengthOfStringObject.cpp
@@ -0,0 +1,15 @@
+/*
+  Author: YUGAM
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	string str = "C++ Programming";
+	
+	cout << "String Length : " << str.size();
+	
+	return 0;
+}
